4fdd21831f55cf470401d7e1e55dbe453d5c8267
Chris Vasterling <chrisvasterling@gmail.com>
2018-03-19 14:07:05 -0500

    search box design

--- js/home.js
+++ js/home.js
@@ -30,24 +30,65 @@ function toggleSearch(id) {
             tog.setAttribute("class", "searchButtonBtn");
             tog.style.opacity = 1;
             setTimeout(function(){
+                runSearch();
                 searchBox.style.display = "none";
                 btn.setAttribute("class", "searchButton");
             }, 300)
         }, 300)
     }
 }
+
 var studentPageList = [
     ["first", "last", ["318", "31415926.."]],
+    ["chris", "last", ["555"]],
+    ["BILLY", "last", ["111"]],
+    ["first", "last", ["99"]],
+    ["first", "lool", ["333", "265"]]
 ]
-function runSearch(id) {
-    toggleSearch(id);
+function runSearch() {
+    var firstName = document.getElementById("firstNameSearch").value.toLowerCase(), 
+        lastName = document.getElementById("lastNameSearch").value.toLowerCase(),
+        student,
+        matchFirst = [], 
+        matchLast = [],
+        matchExact = [], 
+        SRExact = document.getElementById("searchResultExact"),
+        SRFirst = document.getElementById("searchResultFirst"),
+        SRLast = document.getElementById("searchResultLast");
     // perform search
-    // ..List[Y]-Y is induvidual student
+    // ..List[Y]-Y is index of induvidual student
     // ..List[Y][0]-first name
     // ..List[Y][1]-last name
     // ..List[Y][2]-pages student is on
     // ..List[Y][2][X]-X is induvidual page
-    console.log(studentPageList[0][0])
-    console.log(studentPageList[0][1])
-    console.log(studentPageList[0][2])
+    
+    // set default input if not given
+    if (firstName=="") {
+        firstName = "_"
+    }
+    if (lastName=="") {
+        lastName = "_"
+    }
+    
+    // display search criteria stuff
+    SRExact.innerHTML = firstName + " " + lastName;
+    SRFirst.innerHTML = firstName;
+    SRLast.innerHTML = lastName;
+    for (student in studentPageList) {
+        if ((studentPageList[student][0] == firstName)&&(studentPageList[student][1] == lastName)) {
+            // If the first name AND the last name is in the list we found the person
+            matchExact.push(studentPageList[student])
+        } else {
+            if (studentPageList[student][0] == firstName) {
+                // student(s) with first name __
+                matchFirst.push(studentPageList[student]);
+            }
+            if (studentPageList[student][1] == lastName) {
+                // student(s) with last name __
+                matchLast.push(studentPageList[student]);
+            }
+        }
+        
+    }
+    
 }
